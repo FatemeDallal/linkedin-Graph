4410c57ba0aad85abacd690fd0eb290c70a93c5c
FatemeDallal <108277235+FatemeDallal@users.noreply.github.com>
2023-01-20 15:06:26 +0330

    create user

---
+++ User.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, id, name, date_of_birth, university_location, field, work_place, specialties, connection_id):
+        self.id = id
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.university_location = university_location
+        self.field = field
+        self.work_place = work_place
+        self.specialties = specialties
+        self.connection_id = connection_id
+
