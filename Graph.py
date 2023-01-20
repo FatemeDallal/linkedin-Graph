fbe833056828d639d5a7ceca6b6ac17287b525e6
FatemeDallal <108277235+FatemeDallal@users.noreply.github.com>
2023-01-20 15:01:33 +0330

    graph implementation

---
+++ Graph.py
@@ -0,0 +1,16 @@
+class Vertex:
+    def __init__(self, user):
+        self.data = user
+        self.adjV = set()
+
+
+class Graph:
+    def __init__(self):
+        self.vertexes = set()
+
+    def add_edge(self, u, v):
+        u.adjV.add(v)
+        v.adjV.add(u)
+        self.vertexes.add(u)
+        self.vertexes.add(v)
+
