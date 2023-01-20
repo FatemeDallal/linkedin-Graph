1b1a2b7968c97d7ae9eff1ce9c515936c93be1fc
FatemeDallal <108277235+FatemeDallal@users.noreply.github.com>
2023-01-20 17:54:16 +0330

    read json file and create Graph with datas(json file)

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import json
 from Graph import Graph, Vertex
 from User import User
 
+
+def search_id(graph_vertex, id):
+
+    for i in graph_vertex:
+        if i.data.id == id:
+            return i
+
+    return False
+
 if __name__ == '__main__':
 
     d = open('users.json')
@@ -11,22 +20,32 @@ if __name__ == '__main__':
 
     users = []
 
+    # read file
     for i in data:
         users.append(User(
-            i
+            i.get("id"),
+            i.get("name"),
+            i.get("dateOfBirth"),
+            i.get("universityLocation"),
+            i.get("field"),
+            i.get("workplace"),
+            i.get("specialties"),
+            i.get("connectionId")
         ))
 
     d.close()
 
+    graph = Graph()
+
+    # add vertex to graph
+    for i in users:
+        graph.vertexes.add(Vertex(i))
+
+    # add edge to graph
+    for i in graph.vertexes:
+        for j in i.data.connection_id:
+            temp = search_id(graph.vertexes, j)
+            if temp is not False:
+                graph.add_edge(i, temp)
 
 
-    # g = Graph()
-    #
-    # u = Vertex(12)
-    # v = Vertex(13)
-    # a = Vertex(14)
-    # b = Vertex(2)
-    #
-    # g.add_edge(u, v)
-    # g.add_edge(v, a)
-    # g.add_edge(b, a)
